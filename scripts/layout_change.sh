2b8e7e5c0a1b1052f0ebb1f84d2158077335c5e5
vmsynkov <vmsynkov@gmail.com>
2016-04-24 20:51:58 +0300

    layout_change

--- scripts/layout_change.sh
+++ scripts/layout_change.sh
@@ -11,4 +11,6 @@ elif [ $LAYOUT = 'ru' ]; then
 	setxkbmap -layout us -option terminate:ctrl_alt_bksp -option caps:swapescape -option ctrl:swap_lalt_lctl
 fi
 
+xmodmap -e "keycode 135 = Super_L"
+
 pkill -RTMIN+4 i3blocks
