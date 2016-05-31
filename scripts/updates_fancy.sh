29b8f3b7a009a592f66616499f0dca3b2de9ad25
vmsynkov <vmsynkov@gmail.com>
2016-06-01 00:22:38 +0300

    *updates_fancy *alc

--- scripts/updates_fancy.sh
+++ scripts/updates_fancy.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 UPDATES_DUMP=$(checkupdates)
+AUR_UPDATES_DUMP=$(pacaur -k)
+
+[[ -n "$AUR_UPDATES_DUMP" ]] && AUR_UPDATES_NUM=$(echo "$AUR_UPDATES_DUMP" | wc -l) || AUR_UPDATES_NUM=0
 [[ -n "$UPDATES_DUMP" ]] && UPDATES_NUM=$(echo "$UPDATES_DUMP" | wc -l) || UPDATES_NUM=0
+
+UPDATES_PARSED=$(echo "$UPDATES_DUMP" | grep -E -o '([a-zA-Z]+[0-9]?+\-?)+\s')
+AUR_UPDATES_PARSED=$( \
+      for OUT in  $(echo "$AUR_UPDATES_DUMP" | cut -b 9- | grep -E -o '([a-zA-Z]+[0-9]?+\-?)+\s'); do
+        echo "(AUR) $OUT";
+      done)
+
 
 if [ "$BLOCK_BUTTON" = "1" ]; then
 
-	ANSWER=$(echo "$UPDATES_DUMP" | \
-           grep -E -o '([a-zA-Z]+[0-9]?+\-?)+\s' | \
-           rofi -dmenu -lines 15 -columns 2 -width 100 -p "updates avaliable: ")
+  ROFI_ANSWER=$(echo -e "$AUR_UPDATES_PARSED\n""$UPDATES_PARSED" | rofi -dmenu -lines 15 -columns 2 -width 100 -p "updates avaliable: ")
 
-	if [ -n "$ANSWER" ]; then
+	if [ -n "$ROFI_ANSWER" ]; then
 
 		PASSWORD=$(rofi -dmenu -password -p "password: ")
 
-		echo $PASSWORD | sudo -S pacman -Syu --noconfirm &> /dev/null &&\
-		notify-send "Pacman" "Updates installed" -i /usr/share/icons/Adwaita/48x48/emotes/face-devilish.png
+		echo $PASSWORD | sudo -S pacaur -Syu --noconfirm &> /dev/null && \
+		notify-send "Updates" "Upgrade finished" -i /usr/share/icons/Adwaita/48x48/emotes/face-devilish.png
 	fi
 
-	echo "    $UPDATES_NUM "
+	echo "    $UPDATES_NUM : $AUR_UPDATES_NUM "
 
-elif [ "$UPDATES_NUM" = "0" ]; then echo "    0 "
-else echo "    $UPDATES_NUM " && \
-     notify-send "Pacman" "${UPDATES_NUM} new updates" -i /usr/share/icons/Adwaita/48x48/emotes/face-devilish.png
+elif [ "$UPDATES_NUM" = "0" && "AUR_UPDATES_NUM" = "0" ]; then echo "    OK! "
+else echo "    $UPDATES_NUM : $AUR_UPDATES_NUM " && \
+     notify-send "Updates" "\nPacman ${UPDATES_NUM}\nPacaur ${AUR_UPDATES_NUM}" -i /usr/share/icons/Adwaita/48x48/emotes/face-devilish.png
 fi
