bcb917cb28327edc0f83a00bc7d9d97fcc4a6afd
Viktor Synkov <vmsynkov@gmail.com>
2016-09-17 01:22:40 +0300

    create init.sh

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+parted /dev/sda mklabel msdos 
+parted /dev/sda mkpart primary ext4 1MiB 100MiB
+parted /dev/sda mkpart primary ext4 100MiB 8GiB
+parted /dev/sda mkpart primary ext4 8GiB 100%
+mkfs.ext4 /dev/sda1
+mkfs.ext4 /dev/sda2
+mkfs.ext4 /dev/sda3
+
+
+mount /dev/sda2 /mnt
+
+mkdir -p /mnt/home /mnt/boot
+
+mount /dev/sda1 /mnt/boot
+mount /dev/sda3 /mnt/home
+
+pacstrap -i /mnt base base-devel grub
+
+genfstab -U /mnt > /mnt/etc/fstab
