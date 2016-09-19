065c18d5799eb28310e83f11426f64736dda5be7
Viktor Synkov <vmsynkov@gmail.com>
2016-09-19 03:49:34 +0300

    Update init2.sh

--- init2.sh
+++ init2.sh
@@ -23,7 +23,7 @@ useradd -m -g 1993 -u 1304 cli3mo
 echo "root:j" | chpasswd
 echo "cli3mo:j" | chpasswd
 
-echo "cli3mo ALL=(ALL) NOPASSWD:ALL"
+echo "cli3mo ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
 #cp /usr/lib/systemd/system/getty@.service /home/cli3mo/autologin@.service
 #ln -s /home/cli3mo/autologin@.service /etc/systemd/system/getty.target.wants/getty@tty1.service
